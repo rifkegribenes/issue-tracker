ea6377a741187388b571780bac0e53dc7d9b5f99
rifkegribenes <rifkegribenes@gmail.com>
2018-09-30 04:48:16 +0000

    post route

--- routes/api.js
+++ routes/api.js
@@ -14,8 +14,6 @@ var ObjectId = require('mongodb').ObjectID;
 
 const CONNECTION_STRING = process.env.DB; 
 
-//MongoClient.connect(CONNECTION_STRING, function(err, db) {});
-
 module.exports = function (app) {
 
   app.route('/api/issues/:project')
@@ -25,9 +23,29 @@ module.exports = function (app) {
       
     })
     
-    .post(function (req, res){
-      var project = req.params.project;
-      
+    .post((req, res) => {
+      const project = req.params.project;
+      const issue = {
+        issue_title: req.body.issue_title,
+        issue_text: req.body.issue_text,
+        created_on: new Date(),
+        updated_on: new Date(),
+        created_by: req.body.created_by,
+        assigned_to: req.body.assigned_to || '',
+        open: true,
+        status_text: req.body.status_text || ''
+      };
+      if (!issue.issue_title || !issue.issue_text || !issue.created_by) {
+        res.send('missing inputs');
+      } else {
+        MongoClient.connect(CONNECTION_STRING, (err, db) => {
+          const collection = db.collection(project);
+          collection.insertOne(issue, (err,doc) => {
+            issue._id = doc.insertedId;
+            res.json(issue);
+          });
+        });
+      }
     })
     
     .put(function (req, res){
